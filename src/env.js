cc5a3dcfe8f31a5dbaf93669a54667c03691c2b3
GideonKoenig <gideon.koenig@online.de>
2024-03-22 14:35:12 +0100

    first public release

--- src/env.js
+++ src/env.js
@@ -14,6 +14,10 @@ export const env = createEnv({
                 (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
                 "You forgot to change the default URL",
             ),
+        DIRECT_URL: z
+            .string()
+            .url()
+            .optional(),
         NODE_ENV: z
             .enum(["development", "test", "production"])
             .default("development"),
@@ -52,6 +56,7 @@ export const env = createEnv({
      */
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
+        DIRECT_URL: process.env.DIRECT_URL,
         NODE_ENV: process.env.NODE_ENV,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
